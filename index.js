7259103195767f831ea4bae8953c7351d91f2717
Fabrizio Carlassara <53102151+carlassaraf@users.noreply.github.com>
2022-03-08 23:04:51 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,18 +1,3 @@
 "use strict";
 
-/* Variables globales */
-
-function cargar() {
-
-  /* Obtiene la nota */
-  let nota = parseFloat(document.getElementById("nota").value);
-
-  /* TODO */
-
-}
-
-function informar() {
-
-  /* TODO */
-  
-}
+/* TODO */
